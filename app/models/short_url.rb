7ed5d90deb2037bd6522cc948a302ac4616e9083
Loai Al-Sharee <loaialsharee1@gmail.com>
2026-02-08 03:25:10 +0800

    feat(short_url): add has_many :visits association

--- app/models/short_url.rb
+++ app/models/short_url.rb
@@ -1,4 +1,6 @@
 class ShortUrl < ApplicationRecord
   validates :target_url, presence: true
   validates :code, presence: true, uniqueness: true, length: { maximum: 15 }
+
+  has_many :visits, dependent: :destroy
 end
